39831195335370bd7bf40cc100d583d1f2b5bf67
someone5678 <nemui3353@gmail.com>
2023-05-17 17:55:23 +0900

    customapps_backups: InstallScripts: Add Zsh/Oh-My-Zsh setup script
    * Set default shell to Z shell (Z shell is installed by default on SteamOS 3.0).
    * Install Oh My Zsh and custom plugins
    
    Plugins list:
    autoupdate
    zsh-autocomplete
    zsh-autosuggestions
    zsh-syntax-highlighting
    
    Ref:
    Z shell: https://www.zsh.org/
    
    Oh My Zsh:
    https://ohmyz.sh/
    https://github.com/ohmyzsh/ohmyzsh
    
    Oh My Zsh plugins:
    https://github.com/tamcore/autoupdate-oh-my-zsh-plugins
    https://github.com/marlonrichert/zsh-autocomplete
    https://github.com/zsh-users/zsh-autosuggestions
    https://github.com/zsh-users/zsh-syntax-highlighting

---
+++ customapps_backups/SteamDeckSetups/InstallScripts/ZSH/install-zsh-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh -x
+cd /home/deck/.oh-my-zsh/custom/plugins/
+git clone https://github.com/tamcore/autoupdate-oh-my-zsh-plugins autoupdate
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/marlonrichert/zsh-autocomplete
---
+++ customapps_backups/SteamDeckSetups/InstallScripts/ZSH/set-zsh-default.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh -x
+
+chsh -s $(which zsh)
+sudo -s
+chsh -s $(which zsh)
